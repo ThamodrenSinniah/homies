1c987fcc5dd2af62489a0d0f4f1c3a708697a802
thamodrensinniah <thamodren.sinniah@duerrdental.com>
2023-06-25 16:26:00 +0800

    Add headless arguments and comments

--- tests/fixtures/browser.py
+++ tests/fixtures/browser.py
@@ -1,9 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
 
 
 @pytest.fixture(scope="session")
 def browser():
-    driver = webdriver.Firefox()
+    options = Options()
+    # Container does not have GPU so have to run on headless
+    # Feel free to remove when testing locally to see the browser
+    options.add_argument('--headless')
+    driver = webdriver.Firefox(options=options)
     yield driver
     driver.quit()
